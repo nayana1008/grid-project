8f38fd86c6d20e8da1581df0c044548643199358
nayana1008 <71483034+nayana1008@users.noreply.github.com>
2020-09-24 12:30:50 +0530

    Add files via upload

---
+++ dynamic-form/preview-component/preview-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-component',
+  templateUrl: './preview-component.component.html',
+  styleUrls: ['./preview-component.component.css']
+})
+export class PreviewComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
